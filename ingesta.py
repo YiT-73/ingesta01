2a9c3712bcd204e603db895cc2e627d2a53075d4
YiT <yitzhaknamihas@gmail.com>
2025-05-16 23:32:52 -0500

    Update ingesta.py

--- ingesta.py
+++ ingesta.py
@@ -1,7 +1,7 @@
 import boto3
 
 ficheroUpload = "data.csv"
-nombreBucket = "yit-output-01"
+nombreBucket = "yit-output-1"
 
 s3 = boto3.client('s3')
 response = s3.upload_file(ficheroUpload, nombreBucket, ficheroUpload)
